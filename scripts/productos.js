578236aa44666f8597ad72d74ecc0b22204a0b84
joamino <joa2005ush@gmail.com>
2025-06-03 22:28:36 -0300

    Crear scripts/productos.js

---
+++ scripts/productos.js
@@ -0,0 +1,30 @@
+// scripts/productos.js
+
+// Array de productos. Puedes agregar los campos que necesites (id, nombre, precio, imagen, descripción, etc.)
+const productos = [
+  {
+    id: 1,
+    nombre: "Camiseta React",
+    precio: 3500,
+    imagen: "img/camiseta-react.jpg",
+    descripcion: "Camiseta cómoda con logo de React."
+  },
+  {
+    id: 2,
+    nombre: "Taza JavaScript",
+    precio: 1500,
+    imagen: "img/taza-js.jpg",
+    descripcion: "Taza para amantes de JavaScript."
+  },
+  {
+    id: 3,
+    nombre: "Sticker HTML",
+    precio: 500,
+    imagen: "img/sticker-html.png",
+    descripcion: "Sticker resistente al agua con logo HTML5."
+  }
+  // ...agrega más productos aquí
+];
+
+// Si necesitas exportar para usar con módulos ES6:
+// export default productos;
