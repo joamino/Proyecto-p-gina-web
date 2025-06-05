7563e187ff50ffe733cd0dfbe10e48ebe9c7eac4
joamino <joa2005ush@gmail.com>
2025-06-05 12:17:24 -0300

    Actualizar recomendaciones.js

--- scripts/recomendaciones.js
+++ scripts/recomendaciones.js
@@ -72,7 +72,8 @@ function agregarAlCarrito(button) {
   const id = producto.dataset.id;
   const nombre = producto.dataset.nombre;
   const precio = parseFloat(producto.dataset.precio);
-
+const imagen = producto.querySelector("img").src;
+carrito.push({ id, nombre, precio, cantidad: 1, imagen });
   let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
   const existente = carrito.find(item => item.id === id);
   if (existente) {
